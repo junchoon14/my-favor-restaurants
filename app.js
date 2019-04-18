9ecc122b080d6a52a0fe56219169fc0710b8300d
Alvis Hsu <junchoon14@gmail.com>
2019-04-18 22:12:53 +0800

    Add route

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+
+
 mongoose.connect('mongodb://localhost/restauant', { useNewUrlParser: true })
 
 const db = mongoose.connection
@@ -14,9 +16,44 @@ db.once('open', () => {
   console.log('db connected!')
 })
 
+const Restaurant = require('./models/restaurant')
+
+//main
 app.get('/', (req, res) => {
   res.send('index')
 })
+
+app.get('/restaurants', (req, res) => {
+  res.send('index')
+})
+
+// create
+app.get('/restaurants/new', (req, res) => {
+  res.send('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  res.send('add new')
+})
+
+//read
+app.get('/restaurants/:id', (req, res) => {
+  res.send('read')
+})
+
+//edit
+app.get('/restaurants/:id/edit', (req, res) => {
+  res.send('edit')
+})
+app.post('/restaurants/:id', (req, res) => {
+  res.send('edit')
+})
+
+//delete
+app.post('/restaurants:id/delete', (req, res) => {
+  res.send('delete')
+})
+
 
 app.listen(3000, () => {
   console.log('App is running!')
