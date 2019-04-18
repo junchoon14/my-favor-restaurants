a7afb591202a5fd029e00ae6f552baa8d96ecf78
Alvis Hsu <junchoon14@gmail.com>
2019-04-18 22:02:25 +0800

    Create Restaurant model and add mongoose connection

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/restauant', { useNewUrlParser: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('db error!')
+})
+
+db.once('open', () => {
+  console.log('db connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('index')
